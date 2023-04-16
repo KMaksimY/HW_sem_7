47a412ba2aeed8e89206b2fb06f1068114586886
Maksim <komarovmu@gmail.com>
2023-04-16 16:43:49 +0100

    Add Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество строк m: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        double[,] matrix = new double[m, n];
+        Random random = new Random();
+
+        Console.WriteLine("Исходная матрица:");
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = random.NextDouble() * (10 - (-10)) + (-10);
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
